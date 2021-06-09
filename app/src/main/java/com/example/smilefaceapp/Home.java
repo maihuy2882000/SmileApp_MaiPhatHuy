673a2c7cba5f6dfe4536261f63810a2ec6f81f8a
maihuy2882000 <maihuy2882000@gmail.com>
2021-06-09 12:02:08 +0700

    first commit

---
+++ app/src/main/java/com/example/smilefaceapp/Home.java
@@ -0,0 +1,38 @@
+package com.example.smilefaceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class Home extends AppCompatActivity {
+
+    Button home_signin, home_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        home_signin = findViewById(R.id.home_signin);
+        home_register = findViewById(R.id.home_register);
+
+        home_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, SignIn.class));
+            }
+        });
+
+        home_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, Register.class));
+            }
+        });
+    }
+}
